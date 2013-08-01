cbf8e7e2a7f01ee2fe441f345405e66f9bf6f73e
dandan <godvmxi@gmail.com>
2013-08-01 17:04:52 +0800

    Create virtual_serial_linux.py
    add mini tool for linux virtual serial tools ,like VSDP in win

---
+++ virtual_serial_linux.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+#coding=utf-8
+
+import pty
+import os
+import select
+import socket
+
+
+
+def mkpty():
+    #
+    master1, slave = pty.openpty()
+    slaveName1 = os.ttyname(slave)
+    master2, slave = pty.openpty()
+    slaveName2 = os.ttyname(slave)
+    print '\nslave device names: ', slaveName1, slaveName2
+    return master1, master
+
+
+if __name__ == "__main__":
+
+    master1, master2 = mkpty()
+    while True:
+        rl, wl, el = select.select([master1,master2], [], [], 1)
+        for master in rl:
+            data = os.read(master, 128)
+            print "read %d data." % len(data)
+            if master==master1:
+                os.write(master2, data)
+            else:
+                os.write(master1, data)
