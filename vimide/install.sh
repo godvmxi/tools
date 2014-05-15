3ccb996412d8a85e2ec775d6bd1264b405ad3d42
bright.jiang <bright.jiang@infotmic.com.cn>
2014-05-15 16:36:23 +0800

    fix install

--- vimide/install.sh
+++ vimide/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #sudo apt-get install 
- sudo apt-get install exuberant-ctags 
+# sudo apt-get install exuberant-ctags 
 echo "back up your vim config file"
 cp -rf ~/.vimrc  ~/vimrc.bak
 cp -rf ~/.vim/	 ~/vim.bak  
